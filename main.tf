7c1381c3b9cd80bec0246fed7491e18735f8456e
aimaratova003 <aimaratova1@gmail.com>
2024-05-13 20:35:31 -0500

    new

--- main.tf
+++ main.tf
@@ -103,10 +103,24 @@ output "private_subnet_ids" {
 }
 
 #ASG
-resource "aws_launch_template" "asg" {
-  name_prefix   = "asg"
-  image_id      = "ami-0900fe555666598a2"
-  instance_type = "t2.micro"
+resource "aws_security_group" "db-sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "db-sg"
+  description = "Allow access to our DB from anywhere"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_autoscaling_group" "asg" {
